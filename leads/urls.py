f507035ebf6abc4014b624bc9ccd12e7f6920a56
Teboho Manyeli <temanye022@student.wethinkcode.co.za>
2023-07-01 01:18:04 +0200

    long way using LeadForm() to update the lead

--- leads/urls.py
+++ leads/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import lead_list, lead_detail, lead_create
+from .views import lead_list, lead_detail, lead_create, lead_update
 
 app_name = "leads"
 
 
 urlpatterns = [
-    path('',lead_list),                 # http://127.0.0.1:8000/leads/              - default leads landing page 
-    path('create/', lead_create),       # http://127.0.0.1:8000/leads/create/
-    path('<int:pk>/', lead_detail)      # http://127.0.0.1:8000/leads/1/            - shows details for each lead 
+    path('',lead_list),                     # http://127.0.0.1:8000/leads/              - default leads landing page 
+    path('create/', lead_create),           # http://127.0.0.1:8000/leads/create/
+    path('<int:pk>/', lead_detail),         # http://127.0.0.1:8000/leads/1/            - shows details for each lead 
+    path('<int:pk>/update/',lead_update )   # http://127.0.0.1:8000/leads/1/update/ 
 ]
--- leads/views.py
+++ leads/views.py
@@ -34,22 +34,9 @@ def lead_create(request):
 
         if form.is_valid():
             print("The form is valid")
-            # print(form.cleaned_data)
-
-            # first_name = form.cleaned_data['first_name']
-            # last_name = form.cleaned_data['last_name']
-            # age = form.cleaned_data['age']
-            # agent = form.cleaned_data['agent']
-
-            
-            # Lead.objects.create(
-            #     first_name = first_name,
-            #     last_name = last_name,
-            #     age = age,
-            #     agent = agent
-            # )
 
             form.save()
+
             print("Lead has been created")
             return redirect("/leads/")
 
@@ -57,3 +44,37 @@ def lead_create(request):
         "form": form
     }
     return render(request, "leads/lead_create.html", context)
+
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+
+    form = LeadForm()
+
+    if request.method == "POST":        #Proccess the form
+        print('Receiving a post request')
+        form = LeadForm(request.POST)
+
+        if form.is_valid():
+            print("Valid Update form")
+
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+         
+
+            lead.first_name = first_name
+            lead.last_name = last_name
+            lead.age = age
+            lead.save()
+
+            print("Lead has been created")
+            return redirect(f"/leads/{pk}")
+
+    
+    context = {
+        "form": form,
+        "lead":lead
+    }
+    return render(request, "leads/lead_update.html", context)
